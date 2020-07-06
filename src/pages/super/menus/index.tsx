270dd7822f6d969fe659229d67f67c56aceb94e4
tkvern <tkvern@qq.com>
2020-07-06 18:06:09 +0800

    增加拖拽表格

--- src/pages/super/menus/index.tsx
+++ src/pages/super/menus/index.tsx
@@ -1,20 +1,73 @@
+import React from 'react';
+
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import React, { useState, useEffect } from 'react';
-import { Spin } from 'antd';
+import { Card, Col, Row, Button } from 'antd';
+import { PlusSquareOutlined, MinusSquareOutlined } from '@ant-design/icons';
+
+import Nestable from 'antd-nestable';
+
 import styles from './index.less';
 
+const items = [
+  { id: 0, text: 'Andy' },
+  {
+    id: 1,
+    text: 'Harry',
+    children: [{ id: 2, text: 'David' }],
+  },
+  { id: 3, text: 'Lisa' },
+];
+
+const renderItem = (params: any) => params?.item?.text;
+
 export default () => {
-  const [loading, setLoading] = useState<boolean>(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
   return (
     <PageHeaderWrapper content="这是一个新页面，从这里进行开发！" className={styles.main}>
-      <div style={{ paddingTop: 100, textAlign: 'center' }}>
-        <Spin spinning={loading} size="large" />
-      </div>
+      <Row>
+        <Col span={12}>
+          <Card>
+            <div>
+              <Button
+                className={styles.action}
+                type="primary"
+                icon={<PlusSquareOutlined />}
+                onClick={() => {
+                  // collapse(0);
+                }}
+              >
+                Expand
+              </Button>
+              <Button
+                className={styles.action}
+                type="primary"
+                icon={<MinusSquareOutlined />}
+                onClick={() => {
+                  // collapse(1);
+                }}
+              >
+                Collapse
+              </Button>
+              <Button
+                className={styles.action}
+                type="primary"
+                icon={<MinusSquareOutlined />}
+                onClick={() => {
+                  // collapse(2);
+                }}
+              >
+                Collapse2
+              </Button>
+            </div>
+            <Nestable
+              items={items}
+              renderItem={renderItem}
+              onChange={(value: []) => {
+                console.log(value);
+              }}
+            />
+          </Card>
+        </Col>
+      </Row>
     </PageHeaderWrapper>
   );
 };
