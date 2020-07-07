35a3651c234f280db6acf964ce7790eb7b870265
tkvern <tkvern@qq.com>
2020-07-07 10:33:42 +0800

    完善nestable

--- src/pages/super/menus/index.tsx
+++ src/pages/super/menus/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { Card, Col, Row, Button } from 'antd';
@@ -21,6 +21,23 @@ const items = [
 const renderItem = (params: any) => params?.item?.text;
 
 export default () => {
+  const nestableRef = useRef<{ collapse: (type: string | number[]) => void }>();
+  const collapse = (collapseCase: number) => {
+    if (nestableRef.current) {
+      switch (collapseCase) {
+        case 0:
+          nestableRef.current?.collapse('NONE');
+          break;
+        case 1:
+          nestableRef.current?.collapse('ALL');
+          break;
+        case 2:
+          nestableRef.current?.collapse([1]);
+          break;
+        default:
+      }
+    }
+  };
   return (
     <PageHeaderWrapper content="这是一个新页面，从这里进行开发！" className={styles.main}>
       <Row>
@@ -32,7 +49,7 @@ export default () => {
                 type="primary"
                 icon={<PlusSquareOutlined />}
                 onClick={() => {
-                  // collapse(0);
+                  collapse(0);
                 }}
               >
                 Expand
@@ -42,7 +59,7 @@ export default () => {
                 type="primary"
                 icon={<MinusSquareOutlined />}
                 onClick={() => {
-                  // collapse(1);
+                  collapse(1);
                 }}
               >
                 Collapse
@@ -52,13 +69,14 @@ export default () => {
                 type="primary"
                 icon={<MinusSquareOutlined />}
                 onClick={() => {
-                  // collapse(2);
+                  collapse(2);
                 }}
               >
                 Collapse2
               </Button>
             </div>
             <Nestable
+              ref={nestableRef}
               items={items}
               renderItem={renderItem}
               onChange={(value: []) => {
