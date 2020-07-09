60dfd50bfc5af36cb7f05b26e34d34b0bfb6101a
tkvern <tkvern@qq.com>
2020-07-09 16:15:22 +0800

    增加约束

--- src/services/API.d.ts
+++ src/services/API.d.ts
@@ -1,12 +1,19 @@
 declare namespace API {
-  export interface response {
+  export interface response<T = {}> {
     success: boolean; // if request is success
-    data?: any; // response data
+    data: T; // response data
     errorCode?: string; // code for errorType
     errorMessage?: string; // message display to user
     showType?: number; // error display type： 0 silent; 1 message.warn; 2 message.error; 4 notification; 9 page
     traceId?: string; // Convenient for back-end Troubleshooting: unique request ID
     host?: string; // onvenient for backend Troubleshooting: host of current access server
+  }
+
+  export interface paginData<T = {}> {
+    current: number;
+    pageSize: number;
+    total: number;
+    list: T[];
   }
 
   export interface CurrentUser {
